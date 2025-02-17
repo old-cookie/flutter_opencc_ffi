e03af3cfaef816a0c3a18be3251f73a4676fdad1
oldcookie <jxky2004@gmail.com>
2025-02-17 22:57:52 +0800

    1

--- flutter_opencc_ffi/example/android/app/src/main/kotlin/com/whaleread/flutter_opencc_ffi/flutter_opencc_ffi_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.whaleread.flutter_opencc_ffi.flutter_opencc_ffi_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_opencc_ffi_android/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_opencc_ffi_android
-
-See [flutter_opencc_ffi](https://github.com/dolphinxx/flutter_opencc_ffi/tree/master/flutter_opencc_ffi)
